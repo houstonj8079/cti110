36554cc1e03f138e206abdeb5b137927b78cde0a
houstonj8079 <160425208+houstonj8079@users.noreply.github.com>
2024-02-18 14:08:18 -0500

    Add files via upload
    First submission.

---
+++ P1LAB1_HoustonTaylorJordan.py
@@ -0,0 +1,12 @@
+Python 3.12.2 (v3.12.2:6abddd9f6a, Feb  6 2024, 17:02:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Jordan Houston-Taylor
+>>> # February 17,2024
+>>> # P1LAB1 Assignment
+>>> # This assignment teaches students how to create a Python program in IDLE.
+>>> first_name = 'Pat'
+>>> 
+>>> print('Hey', first_name)
+Hey Pat
+>>> print('Welcome to zyBooks!')
+Welcome to zyBooks!
