1c70aac795c2ee10479da95061c571fa2df697c6
houstonj8079 <160425208+houstonj8079@users.noreply.github.com>
2024-02-19 21:13:09 -0500

    Update P1LAB1_HoustonTaylorJordan.py
    Third upload, forgot to add the string in the second one

--- P1LAB1_HoustonTaylorJordan.py
+++ P1LAB1_HoustonTaylorJordan.py
@@ -2,6 +2,7 @@
 # February 17, 2024
 # P1LAB1 Assignment
 # This assignment teaches students how to create a Python program in IDLE
-first_name = 'Pat'
+first_name= (input('Enter your first name:'), end=' ')
 print ('Hey', first_name)
 print ('Welcome to zyBooks!')
+
