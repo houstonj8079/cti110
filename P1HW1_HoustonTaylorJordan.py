4111f525cf8e2a7c83b046e0108a52b39050d5cb
houstonj8079 <160425208+houstonj8079@users.noreply.github.com>
2024-04-28 18:45:38 -0400

    Add files via upload

---
+++ P1HW1_HoustonTaylorJordan.py
@@ -0,0 +1,22 @@
+# Jordan Houston-Taylor
+# February 25, 2024
+# P1HW1 Assignment
+# The purpose of this project to create a python code that will be able to collect and process information.
+
+user_num = int(input('Enter integer:\n'))
+
+print (f"You entered: {user_num}")
+
+squared = user_num * user_num
+cubed = user_num * user_num * user_num
+
+print (f"{user_num} squared is {squared}")
+print (f"And {user_num} cubed is {cubed} !!")
+
+user_num2 = int(input('Enter another integer:\n'))
+
+sum = user_num2 + 4
+product = user_num2 * 4
+
+print (f"4 + {user_num2} is {sum}")
+print (f"4 * {user_num2} is {product}")
