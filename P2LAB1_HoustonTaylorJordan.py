622f60bb3f64cdbf8f83852112e1ac734779ff8f
houstonj8079 <160425208+houstonj8079@users.noreply.github.com>
2024-04-28 18:42:27 -0400

    Add files via upload

---
+++ P2LAB1_HoustonTaylorJordan.py
@@ -0,0 +1,18 @@
+# Jordan Houston-Taylor
+
+# March 3, 2024
+
+# P2LAB1 Assignment 
+
+# This assignment will help me learn how to use floating-point inputs for decimal values or fractional numbers.
+
+
+miles = float(input())
+gas = float(input())
+mpg = gas/miles
+
+your_value1= (mpg)* 20
+your_value2= (mpg)* 75
+your_value3= (mpg)* 500
+
+print(f'{your_value1:.2f} {your_value2:.2f} {your_value3:.2f}')
